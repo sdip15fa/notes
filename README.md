b115f6a6a7760068818cb7d2d53b4c755a37378d
wcyat <9657150-wcyat@users.noreply.gitlab.com>
2021-11-25 22:40:54 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 A simple notes sharing system. <br>
 [https://notes.wcyat.me](https://notes.wcyat.me)
+
+# Deploy
+```
+npm install
+npm run start
+```
+the server will start at localhost:4000 (you need to set two environmental variables, mongocred: username:password, mongourl for mongodb)
+
+Then you should host your static content elsewhere, remember to change the links.
+
+# Notes
+idgenerator is hosted by cloudflare workers.
---
+++ idgenerator/idgenerator.js
@@ -0,0 +1,93 @@
+addEventListener("fetch", (event) => {
+    event.respondWith(
+      handleRequest(event.request).catch(
+        (err) => new Response(err.stack, { status: 500 })
+      )
+    );
+  });
+  
+  /**
+   * Many more examples available at:
+   *   https://developers.cloudflare.com/workers/examples
+   * @param {Request} request
+   * @returns {Promise<Response>}
+   */
+  
+   let options = {
+      include: {
+          numbers: true,
+          upper: false,
+          lower: true,
+          special: false,
+      },
+      digits: 6,
+  };
+  let ld = {
+      numbers: 3,
+      upper: 2,
+      lower: 1.5,
+      special: 2.5
+  }
+  const lists = {
+      numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+      upper: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+      lower: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+      special: ['!', '#', '$', '%', '@', '*', '?'],
+  }
+  
+  function grand (list) {
+      let o = Math.floor(Math.random() * list.length);
+      return list[o];
+  }
+  
+  function generate () {
+      let amount = {
+          numbers: 0,
+          upper: 0,
+          lower: 0,
+          special: 0
+      };
+      let digits = options["digits"];
+      for (i in options.include) {
+          if  (options.include[i]) {
+              amount[i] = 1;
+              digits--;
+          }
+      }
+      for (i in options.include) {
+          if (digits < 0) {
+              break;
+          }
+          else if (options.include[i]) {
+              amount[i] += Math.floor(Math.random() * digits / ld[i]);
+              digits -= amount[i] - 1;
+          }
+      }
+      for (i in options.include) {
+          if (options.include[i]) {
+              amount[i] += digits;
+              break; 
+          };
+      };
+      let olist = ['numbers', 'upper', 'lower', 'special'];
+      let output = "";
+      let r = 4;
+      for (let digits = options["digits"]; digits > 0 ; digits--) {
+          let o = olist[Math.floor(Math.random() * r)];
+          if (amount[o] > 0) {
+              output += grand(lists[o]);
+              amount[o]--;
+          }
+          else {
+              olist.splice(olist.indexOf(o), 1);
+              r--;
+              digits++;
+          }
+      }
+      return output;
+  }
+  async function handleRequest(request) {
+    let response = new Response(generate());
+    response.headers.set('Access-Control-Allow-Origin', '*')
+    return response;
+  }
