bdb422fc8ffef0fd61e96e5cf055c0fb90ac915f
wcyat <9657150-wcyat@users.noreply.gitlab.com>
2021-11-26 23:49:21 +0800

    update

--- notes.js
+++ notes.js
@@ -52,7 +52,7 @@ app.post("/users/:i", body_parser.json(), async function(req,res) {
             const users = await database.collection('users');
             console.log(await users.find({username : req.body.username}).count())
             if (await users.find({username : req.body.username}).count() > 0) {
-                res.set(406);
+                res.status(406);
                 res.send("Username already used.");
             }
             else {
