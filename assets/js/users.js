417be5b1b27c08f956970bdc943d6c8da3d8d8ee
wcyat <9657150-wcyat@users.noreply.gitlab.com>
2021-11-28 13:35:13 +0800

    update

--- assets/js/users.js
+++ assets/js/users.js
@@ -1,39 +1,40 @@
 if (localStorage.key && localStorage.username) {
-    window.location.replace("../?signedin=true");
+  window.location.replace("../?signedin=true");
 }
 
 function signup () {
-    if (!document.getElementById('username').value || !document.getElementById('password').value) {
-        document.getElementById('warning').innerHTML = "Username / password cannot be empty.";
-        return;
-    }
-    axios.post("https://notes-server.wcyat.me/users/signup", {username : document.getElementById('username').value, password : document.getElementById('password').value})
-      .then(async function(res) {
-          localStorage.username = document.getElementById('username').value;
-          localStorage.key = await res.data;
-          window.location.replace("../?signup=successful");
-      })
-      .catch(function(error) {
-          document.getElementById('warning').innerHTML = error.response.data;
-      })
+  if (!document.getElementById('username').value || !document.getElementById('password').value) {
+    document.getElementById('warning').innerHTML = "Username / password cannot be empty.";
+    return;
+  }
+  axios.post("https://notes-server.wcyat.me/users/signup", {username : document.getElementById('username').value, password : document.getElementById('password').value})
+    .then(async function(res) {
+      localStorage.username = document.getElementById('username').value;
+      localStorage.key = await res.data;
+      window.location.replace("../?signup=successful");
+    })
+    .catch(function(error) {
+      document.getElementById('warning').innerHTML = error.response.data;
+    })
 }
 
 function signin () {
-    axios.post("https://notes-server.wcyat.me/users/signin", {username : document.getElementById('username').value, password : document.getElementById('password').value})
-      .then(async function (res) {
-          localStorage.key = await res.data;
-          window.location.replace("../?signin=successful");
-      })
-      .catch(function (error) {
-          document.getElementById('warning').innerHTML = error.response.data;
-      })
+  axios.post("https://notes-server.wcyat.me/users/signin", {username : document.getElementById('username').value, password : document.getElementById('password').value})
+    .then(async function (res) {
+      localStorage.key = await res.data;
+      localStorage.username = document.getElementById('username').value;
+      window.location.replace("../?signin=successful");
+    })
+    .catch(function (error) {
+      document.getElementById('warning').innerHTML = error.response.data;
+    })
 }
 
 function check (id) {
-    if (!document.getElementById(id).value) {
-        document.getElementById('warning').innerHTML = "Username / password cannot be empty."
-    }
-    else {
-        document.getElementById('warning').innerHTML = "";
-    }
+  if (!document.getElementById(id).value) {
+    document.getElementById('warning').innerHTML = "Username / password cannot be empty."
+  }
+  else {
+    document.getElementById('warning').innerHTML = "";
+  }
 }
