79764f90ae813229b87d0306cdaebf41858dd4cc
wcyat <9657150-wcyat@users.noreply.gitlab.com>
2021-12-05 13:41:56 +0800

    update

--- compress.sh
+++ compress.sh
@@ -1,3 +1,3 @@
 npm install -g uglify-js
-uglifyjs assets/js/index.js -o assets/js/index.min.js
-uglifyjs assets/js/users.js -o assets/js/users.min.js
+uglifyjs assets/js/index.js -o assets/js/index.min.js --source-map "root='https://notes.wcyat.me/assets/js/',url='index.min.js.map'"
+uglifyjs assets/js/users.js -o assets/js/users.min.js --source-map "root='https://notes.wcyat.me/assets/js/',url='users.min.js.map'"
